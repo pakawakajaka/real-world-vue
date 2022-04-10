5f2875f0f9350ebdd6b729d5e5d82431b26ca259
Austin Lane <austinlane@Austins-Air.fios-router.home>
2022-04-10 18:34:04 -0400

    finished router lesson

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <template>
   <nav>
-    <router-link :to="{ name: 'EventList' }">Home</router-link> |
+    <div id="flashMessage" v-if="GStore.flashMessage">
+      {{ GStore.flashMessage }}
+    </div>
+    <router-link :to="{ name: 'EventList' }">Events</router-link> |
     <router-link :to="{ name: 'About' }">About</router-link>
   </nav>
   <router-view />
 </template>
 
+<script>
+export default {
+  inject: ['GStore'],
+}
+</script>
 <style>
+@keyframes yellowfade {
+  from {
+    background: yellow;
+  }
+  to {
+    background: transparent;
+  }
+}
+
+#flashMessage {
+  animation-name: yellowfade;
+  animation-duration: 3s;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
